99a1cb9fd35eebb7c4e7fb45e9c6193a346122e3
AntonKolev007 <antonkolev@rocketmail.com>
2024-08-14 20:32:25 +0300

    Moving to e-com v2
    Pushing admin functionalities
    Pushing order functionalities
    Pushing addresses functionalities
    Enabling profile, cart, order, address views
    Fixing logics
    Locale handlers for signup dto - experimental functionality
    Reworked front page
    Having scheduled task to remind users for non-ordered stuff
    Fixed visuals
    Added better i18n support
    Minor adjustments

--- src/main/java/com/ecommerce/project/service/impl/ScheduledTaskService.java
+++ src/main/java/com/ecommerce/project/service/impl/ScheduledTaskService.java
@@ -1,45 +1,45 @@
-//package com.ecommerce.project.service.impl;
-//
-//import com.ecommerce.project.model.Cart;
-//import com.ecommerce.project.repositories.CartRepository;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//public class ScheduledTaskService {
-//
-//    private final CartRepository cartRepository;
-//
-//    public ScheduledTaskService(CartRepository cartRepository) {
-//        this.cartRepository = cartRepository;
-//    }
-//
-//    @Scheduled(cron = "0 * * * * ?") // Runs every minute
-//    public void checkNonOrderedCarts() {
-//        processNonOrderedCarts();
-//    }
-//
-//    public List<String> processNonOrderedCarts() {
-//        List<Cart> nonOrderedCarts = cartRepository.findNonOrderedCarts();
-//        List<String> messages = new ArrayList<>();
-//
-//        if (nonOrderedCarts.isEmpty()) {
-//            String message = "No non-ordered carts found.";
-//            System.out.println(message);
-//            messages.add(message);
-//            return messages;
-//        }
-//
-//        nonOrderedCarts.forEach(cart -> {
-//            String emailId = cart.getUser().getEmail();
-//            String message = "Sending reminder to " + emailId + ": You have items in your cart waiting to be ordered.";
-//            System.out.println(message);
-//            messages.add(message);
-//        });
-//
-//        return messages;
-//    }
-//}
+package com.ecommerce.project.service.impl;
+
+import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.repositories.CartRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ScheduledTaskService {
+
+    private final CartRepository cartRepository;
+
+    public ScheduledTaskService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    @Scheduled(cron = "0 * * * * ?") // Runs every minute
+    public void checkNonOrderedCarts() {
+        processNonOrderedCarts();
+    }
+
+    public List<String> processNonOrderedCarts() {
+        List<Cart> nonOrderedCarts = cartRepository.findNonOrderedCarts();
+        List<String> messages = new ArrayList<>();
+
+        if (nonOrderedCarts.isEmpty()) {
+            String message = "No non-ordered carts found.";
+            System.out.println(message);
+            messages.add(message);
+            return messages;
+        }
+
+        nonOrderedCarts.forEach(cart -> {
+            String emailId = cart.getUser().getEmail();
+            String message = "Sending reminder to " + emailId + ": You have items in your cart waiting to be ordered.";
+            System.out.println(message);
+            messages.add(message);
+        });
+
+        return messages;
+    }
+}
